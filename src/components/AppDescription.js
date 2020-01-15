662149b95b090b22833b670d1fc94d8e2f3bce70
Andrew Davisson <akdavisson4@gmail.com>
2020-01-15 09:04:23 -0700

    Add Description and change title

---
+++ src/components/AppDescription.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AppDescription = () => {
+  return (
+    <p>This app is here to just play around with different concepts in React.</p>
+  );
+}
+ 
+export default AppDescription;
