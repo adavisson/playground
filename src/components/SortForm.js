a829af1ecd640c06a8c88e89690a7ea75a8e2a1c
Andrew Davisson <akdavisson4@gmail.com>
2020-01-17 10:12:26 -0700

    Added text

--- src/components/SortForm.js
+++ src/components/SortForm.js
@@ -5,7 +5,8 @@ const SortForm = (props) => {
   return (
     <form onSubmit={props.onSubmit}>
       <label>Size of Array: </label>
-      <input value={props.size} type="number" onChange={props.onChange} min="0" max="15000"/><br/>
+      <input value={props.size} type="number" onChange={props.onChange} min="0" max="15000"/>
+      <> Maximum Size: 15,000</><br/>
       <input type="submit" value="Sort" />
     </form>
   );
