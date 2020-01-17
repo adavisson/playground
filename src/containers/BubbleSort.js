bd302cfe7c9c53aa5f9680a80080cf5eb9875e0f
Andrew Davisson <akdavisson4@gmail.com>
2020-01-17 09:44:31 -0700

    Bubble sort function is implemented

--- src/containers/BubbleSort.js
+++ src/containers/BubbleSort.js
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const BubbleSort = (props) => {
-  const [time,setTime] = useState(0);
+  const [time,setTime] = useState(Date.now() * 0);
+  const [status, setStatus] = useState("Not Started");
+
+  useEffect(() => {
+    if ((props.array.length !== 0) && (status !== "Sorting...")){
+      setStatus("Sorting...");
+      //run bubble sort function
+      setStatus("Sorted", bubbleSort(props.array));
+    }
+  },[props.array])
+
+  const bubbleSort = arr => {
+    const t1 = Date.now();
+    const n = arr.length
+
+    for (let i = 0; i < n; i++){
+      for (let j = 0; j < n-i-1; j++){
+        if (arr[j] > arr[j+1]){
+          const temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      }
+    }
+    console.log(arr);
+
+    const t2 = Date.now();
+    setTime(Math.floor((t2 - t1)));
+    console.log(time);
+    //setStatus("Sorted");
+  }
 
   return (
     <div>
       <h5>BubbleSort</h5>
-      <p>Elapsed Time: {time}</p>
+      <p>Status: {status}</p>
+      <p>Elapsed Time: {time} milliseconds</p>
     </div>
   );
 }
--- src/containers/QuickSort.js
+++ src/containers/QuickSort.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const QuickSort = (props) => {
   const [time, setTime] = useState(0);
+  const [status, setStatus] = useState("Not Started");
 
   return (
     <div>
       <h5>QuickSort</h5>
-      <p>Elapsed Time: {time}</p>
+      <p>Status: {status}</p>
+      <p>Elapsed Time: {time} seconds</p>
     </div>
   );
 }
