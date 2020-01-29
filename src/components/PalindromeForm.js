957cb093a59122488e966c04a1c6733d6e9af331
Andrew Davisson <akdavisson4@gmail.com>
2020-01-29 09:51:01 -0700

    palindrome function works. need to work on ret val

--- src/components/PalindromeForm.js
+++ src/components/PalindromeForm.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const PalindromeForm = (props) => {
   return (
-    <form onSubmit={props.onSubmit}>
+    <form onSubmit={props.handleSubmit}>
       <label>Enter a word.</label><br/>
-      <input type="text" value={props.palWord} onChange={props.onChange} />
+      <input type="text" value={props.palWord} onChange={props.handleChange} />
       <input type="submit" value="Submit" />
     </form>
   );
--- src/containers/Palindrome.js
+++ src/containers/Palindrome.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PalindromeForm from '../components/PalindromeForm';
 
-const desc = () => {
-  return (
-    <p>This component takes a word and checks to see if that word is a palindrome.</p>
-  )
-}
-
 const Palindrome = () => {
+  const [palWord, setPalWord] = useState("");
+
+  const desc = () => {
+    return (
+      <p>This component takes a word and checks to see if that word is a palindrome.</p>
+    )
+  }
+
+  const handleChange = (e) => {
+    setPalWord(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const result = palTest(palWord);
+    console.log(result);
+  }
+
+  const palTest = (input) => {
+    console.log(input);
+    console.log(palWord)
+    const arr = input.split("");
+
+    if (arr.length <= 1) {
+      console.log("success");
+      return `${palWord} is a Palindrome.`
+    }
+
+    const firstLetter = arr.shift();
+    const lastLetter = arr.pop();
+
+    if (firstLetter.toLowerCase() == lastLetter.toLowerCase()){
+      palTest(arr.join(""));
+    } else {
+      console.log("failure");
+      return `${palWord} is not a Palindrome.`
+    }
+  }
+
   return (
     <div className="border">
       {desc()}
-      <PalindromeForm />
+      <PalindromeForm palWord={palWord} handleChange={handleChange} handleSubmit={handleSubmit}/>
     </div>
   );
 }
